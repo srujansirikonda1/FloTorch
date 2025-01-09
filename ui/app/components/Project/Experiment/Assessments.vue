0c802c3bc03acca9dba5e032e1b7b1e6c346cc71
abdul-fission <shaikabdul.gafoor@fissionlabs.com>
2025-01-09 18:06:53 +0530

    feat: added question metrics columns for guardrails in experiment detail page (#87)

---
+++ ui/app/components/Project/Experiment/Assessments.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+    const props = defineProps<{
+    label : string
+    assessments : any
+    }>()
+    
+    const isOpen = ref(false)
+
+</script>
+
+
+<template>
+  <a href="#" class="text-blue-500 hover:underline" @click.prevent="isOpen = true">View</a>
+  <UModal class="overflow-y-scroll max-h-[50vh]" v-model:open="isOpen" :title="props.label">
+    <template #body>
+      <div class="w-full">
+      {{JSON.parse(JSON.stringify(props.assessments))}}
+      </div>
+    </template>
+  </UModal>
+</template>
