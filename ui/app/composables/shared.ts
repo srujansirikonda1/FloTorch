3dc4326a624ade866ff0b126c7e9b5d75dd5cc8f
nandateja <nanda2689@gmail.com>
2025-02-11 14:17:28 +0530

    Chore/update deepseek instance (#222)
    * chore: updated instance type for qwen 1.5B and qwen7B
    
    * fix: update instance type for deepseek r1 distill qwen 1.5B and qwen 7B
    
    * fix: update model labels for deepseek distill r1
    
    ---------
    
    Co-authored-by: nanda.teja1 <nanda.teja@fissionlabs.com>

--- ui/app/composables/shared.ts
+++ ui/app/composables/shared.ts
@@ -414,21 +414,21 @@ export const useProjectCreateMeta = () => {
           model_name : "ml.g5.2xlarge"
         },
         {
-          label: "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B (ml.g5.2xlarge)",
+          label: "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B (ml.g5.xlarge)",
           value: "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
           service: "sagemaker",
           type: "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
           model_name : "ml.g5.xlarge"
         },
         {
-          label: "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B (ml.g5.2xlarge)",
+          label: "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B (ml.g5.xlarge)",
           value: "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
           service: "sagemaker",
           type: "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
           model_name : "ml.g5.xlarge"
         },
         {
-          label: "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B (ml.p4d.24xlarge)",
+          label: "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B (ml.g6e.12xlarge)",
           value: "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B",
           service: "sagemaker",
           type: "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B",
